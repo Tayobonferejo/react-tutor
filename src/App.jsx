b25d06282ce6e1a6f927fb533c2803113b8976d8
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-04 23:30:34 +0100

    more commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import Student from "./Student.jsx"
+import UserGreeting from "./UserGreeting.jsx"
 import { Fragment } from "react";
 function App() {
 
   return (
     <Fragment>
-        <Student name={"Spongebob"} age="80"
-        isStudent = {true}/>
-        <Student name={"timeout"} age={50} isStudent = {false}/>
-        <Student age={30} />
-        <Student></Student>
-        <Student name={1234} age="40" isStudent="true" />
+        <UserGreeting IsLoggedIn={true} username="Brocode"/>
 
     </Fragment>
   );
