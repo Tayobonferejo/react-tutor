5f81ff93a98b8c497ad3e79346ad582be380a407
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-05 22:20:28 +0100

    more commit

--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,9 @@
 function List () {
 
-    const fruits = ["apple", "orange" , "coconut", "pineapple"];
+    const fruits = [{name: "apple", calorie: 95},{name: "orange", calorie: 95} , {name: "banana", calorie: 45},{name: "coconut", calorie : 66}, {name: "pineapple", calorie: 78}];
 
-    return(fruits);
+    const listItems = fruits.map(fruit => <li>{fruit}</li>)
+    return(<ul>{listItems}</ul>);
 }
 
 export default List
