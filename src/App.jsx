d70404b318a3c3edd7fb5f8d1bad2ec3798553b7
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-07 15:53:07 +0100

    more commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import List from "./List.jsx"
+import Button from "./Button.jsx";
+
 function App() {
 
   return (
-    <List></List>
+    <Button></Button>
   );
 }
 
--- src/Button.jsx
+++ src/Button.jsx
@@ -5,11 +5,21 @@ function Button() {
         backgroundColor: "blue",
         color: "white",
         padding: "9px 20px",
+        margin: "2px",
         border: "none",
         cursor: "pointer",
      }
+
+   const handleClick = (e) => e.target.textContent = "OUCH";
+
+   const handleClick2 = (name) => alert(`${name} stop clciking me`)
      
-    return(<button style={button}>Click me</button>)
+    return(
+    <>
+      <button onClick={ () => handleClick2("Bro")} style={button}>Click me</button>
+      <button onClick={(e) => handleClick(e)} style={button}>Click me </button>
+    </>
+   )
 }
 
 export default Button
